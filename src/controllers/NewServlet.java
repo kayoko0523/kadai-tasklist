0696bc717ded7e99041e1d4106570973eddc3984
kayoko.tanaka <kayo.t0422@gmail.com>
2022-11-07 16:36:59 +0900

    NewServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Message;
 import utils.DBUtil;
 
 /**
@@ -32,10 +34,32 @@ public class NewServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
         em.getTransaction().begin();
+        // Messageのインスタンスを生成
+        Message m = new Message();
 
+        // mの各フィールドにデータを代入
+        String title = "taro";
+        m.setTitle(title);
 
+        String content = "hello";
+        m.setContent(content);
 
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        m.setCreated_at(currentTime);
+        m.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.persist(m);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(m.getId()).toString());
 
         em.close();
-    }}
+
+
+
+    }
+
+}
 
